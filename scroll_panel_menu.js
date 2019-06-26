a6d4b646c04127cd1a1e5d9c923ec8c9fb347703
HAEIN KANG <47474328+haeinkang@users.noreply.github.com>
2019-06-26 20:02:56 +0900

    haein [ Enhancement Menu ] 2019-06-26

--- scroll_panel_menu.js
+++ scroll_panel_menu.js
@@ -1,4 +1,19 @@
 $(document).ready(function(){
-	var width = $(window).width();
-	$('.menu_panel').css({width:width});
+	var width = $('.menu_panel').width()/$('.menu').length - 40;
+	$('.menu').css({width:width});
+
+	width = $('.menu').width() * $('.menu').length + 10;
+	$('.menu_box').css({width:width});
+
+	var height = $('.menu').height();
+	$('.menu_box').css({height:height});
+
+	var left = ($('.menu_panel').width() - $('.menu_box').width())/2;
+	$('.menu_box').css({left:left});
+
+	var top = ($('.menu_panel').height() - $('.menu_box').height())/2;
+	$('.menu_box').css({top:top});
+
+
+
 });
